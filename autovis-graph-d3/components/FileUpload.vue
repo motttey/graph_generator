5446c7d0b775a0000583627df67de2b52e0b423c
motttey <motitago@gmail.com>
2020-07-08 09:22:39 +0900

    showing v-data-table below graph

--- autovis-graph-d3/components/FileUpload.vue
+++ autovis-graph-d3/components/FileUpload.vue
@@ -5,7 +5,23 @@
         </label>
         <p id="error">csv ファイルのみアップロード可能です</p>
         <barchart ref="barchart" :data="data" :options="options"></barchart>
+        <v-data-table
+           :headers="headers"
+           :items="csv_data"
+           :items-per-page="5"
+           class="elevation-1"
+         >
+         <template v-slot:items="props">
+           <td class="text-xs-right">{{ props.item.A }}</td>
+           <td class="text-xs-right">{{ props.item.B }}</td>
+           <td class="text-xs-right">{{ props.item.C }}</td>
+           <td class="text-xs-right">{{ props.item.D }}</td>
+           <td class="text-xs-right">{{ props.item.E }}</td>
+           <td class="text-xs-right">{{ props.item.F }}</td>
+         </template>
+       </v-data-table>
     </div>
+
 </template>
 
 <script>
@@ -15,8 +31,17 @@ export default {
   components: { Barchart },
   data: function() {
     return {
-      csv_labels: [""],
-      csv_data: [""],
+      headers: [
+            { text: 'A', align: 'center', sortable: true, value: 'A' },
+            { text: 'B', align: 'center', sortable: true, value: 'B' },
+            { text: 'C', align: 'center', sortable: true, value: 'C' },
+            { text: 'D', align: 'center', sortable: true, value: 'D' },
+            { text: 'E', align: 'center', sortable: true, value: 'E' },
+            { text: 'F', align: 'center', sortable: true, value: 'F' },
+          ],
+      csv_data: [
+        {A: 20, B: 50, C: 30, D: 50, E: 45, F: 20}
+      ],
       data: {
         labels: ['A', 'B', 'C', 'D', 'E', 'F'],
         datasets: [
